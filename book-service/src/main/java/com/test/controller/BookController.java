6c3ca670edb2bc8d343d9a098ef5d3651b377bb1
L <123456@qq.com>
2023-09-22 16:23:01 +0800

    加入了注册与发现

--- book-service/src/main/java/com/test/controller/BookController.java
+++ book-service/src/main/java/com/test/controller/BookController.java
@@ -16,6 +16,7 @@ public class BookController {
 
     @RequestMapping("/book/{bid}")
     Book findBookById(@PathVariable("bid") int bid){
+        System.out.println("你是");
         return service.getBookById(bid);
     }
 }
